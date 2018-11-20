18c49d5cef406a99e88d2137fee61f0ce319de03
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2018-11-20 08:59:53 -0500

    :bear: some styling of Animal Details
    Part 1: Animals

--- src/components/AnimalDetails.js
+++ src/components/AnimalDetails.js
@@ -1,10 +1,23 @@
 import React, { PureComponent } from 'react'
+import styled from 'styled-components'
+
+const DetailList = styled.ul`
+  list-style: none;
+`
+const Detail = styled.span`
+  font-style: italic;
+`
 export default class AnimalDetails extends PureComponent {
   render() {
+    const { Title, CollectiveNoun, Family } = this.props
     return (
-       <ul>
-
-       </ul>
+      <div>
+        <h2>{Title}</h2>
+        <DetailList>
+          <li>Collective Noun: <Detail>{CollectiveNoun}</Detail></li>
+          <li>Family: <Detail>{Family}</Detail></li>
+        </DetailList>
+      </div>
     )
   }
 }
